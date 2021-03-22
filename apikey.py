3a4c21b415eebfc7af8828ceea98d7278843022d
cal284 <76658902+cal284@users.noreply.github.com>
2021-03-22 14:49:50 -0400

    Update apikey.py

--- apikey.py
+++ apikey.py
@@ -1,3 +1,3 @@
 SENDGRID_API_KEY=['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']
-TO_EMAIL = 'mail@examples.com'
+TO_EMAIL = 'fovase5329@990ys.com'
 FROM_EMAIL = 'mail2@examples.com'
