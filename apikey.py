71974752ae30d6cfa5dc7580bde4a673043b9e24
cal284 <76658902+cal284@users.noreply.github.com>
2021-03-22 12:38:26 -0400

    Update apikey.py

--- apikey.py
+++ apikey.py
@@ -1,3 +1,3 @@
-SENDGRID_API_KEY=['Get Your API key from sendgrid']
+SENDGRID_API_KEY=['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']
 TO_EMAIL = 'mail@examples.com'
 FROM_EMAIL = 'mail2@examples.com'
